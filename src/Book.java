0f7f37771a1786b6ab49a3ec6552f5c9269559d2
Vlad <zitarvlad11@gmail.com>
2023-03-16 11:47:09 +0600

    Library

---
+++ src/Book.java
@@ -0,0 +1,48 @@
+import java.util.Objects;
+
+public class Book {
+    private String name;
+    private int yearOfPublication;
+    private Author author;
+
+    public Book(String name, int yearOfPublication, Author author) {
+        this.name = name;
+        this.yearOfPublication = yearOfPublication;
+        this.author = author;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги - " + this.name + " Год издания книги - " + this.yearOfPublication + " " +
+                author.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, yearOfPublication, author);
+    }
+}
